47e543309385e0063b31ae2f1d7cecdaf2e441fe
RebeccaLafont <105719827+RebeccaLafont@users.noreply.github.com>
2022-06-06 12:28:54 +0200

    Synfire chain with imposed burst of 4 spikes
    Before adding adaptation, impose 4 spikes per burst for each layer of neurons.

--- synfirechain4spikes.py
+++ synfirechain4spikes.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #synfirechain with spike 4 times hardcoded
-#SYNFIRE CHAIN
+
 #constants
 s = second = 1.0
 ms = millisecond = 0.001
@@ -29,47 +29,41 @@ V_rest  = -60
 V_reset = -55
 V_spike = -50
 # building the model
-V1 = np.zeros((layer_number, layer_size,n_iterations))-60 
-#why are we reshaping? Any ideas?
-V = V1.reshape(layer_number*layer_size,n_iterations) 
-
-
+V1 = np.zeros((layer_number, layer_size,n_iterations))-60 #matrix of matrix
+V = V1.reshape(layer_number*layer_size,n_iterations) #matrix
 t1 = np.linspace(0, duration1, n_iterations)
 I = np.zeros((layer_number*layer_size, n_iterations))
-I[:15,:200] = 60  #external current given only for the first layer, for 5ms
+I[:15,:50] = 30  #external current given only for the first layer, for 5ms
 
 W1 = np.zeros((layer_number, layer_size, layer_number, layer_size))
+#noise
 noise_mean = 0
 noise_std = 10
 noise_scale = np.sqrt(10*ms)
-
 noise = np.random.normal(noise_mean, noise_std, n_iterations)
-#plt.plot(noise*noise_scale)
-#plt.plot(noise)
 
+'''Creating a weight matrix, where the layer are all connected by the same weight,
+ except between the 4th and 5th parameter which is given as a parameter'''
 def synaptic_weight1(layer_number,layer_size,weight):
   W1 = np.zeros((layer_number, layer_size, layer_number, layer_size))
   for i in range(layer_number-1):
-      W1[i+1,:,i,:]=weight
+      W1[i+1,:,i,:]=1.5
   W1[0,:,-1,:] = 0
   neuron_nr1=layer_number*layer_size #number of neuron per layer
   W1=W1.reshape(neuron_nr1,neuron_nr1)#shape of layers
+  W1[60:75,45:60]=weight #weight as parameter between the 4th and 5th layer
   return W1, neuron_nr1
 
 W2,neuron_nr1=synaptic_weight1(layer_number,layer_size,3)
 
-
-#plt.imshow(W)
-
+''' Create a function recording if there is a spike and the corresponding time
+for the first timestep'''
 def create_all_spikes1(V=V, dt=dt, V_spike=V_spike):
   spikes_yn1=(V[:,1]>=V_spike) #spike_yn1 is a vector 1*150
   last_spikes1=np.zeros(len(spikes_yn1)) #last_spikes1 is a vector 1*150
   for j in range (len(spikes_yn1)):
       if spikes_yn1[j]==1:
         last_spikes1[j]=dt
-      else:
-        last_spikes1[j]=last_spikes1[j]
-
   all_spikes1=np.column_stack((last_spikes1,last_spikes1)) #all_spikes_yn is a matrix (150*2)
   all_spikes1=all_spikes1.tolist()
   return all_spikes1, spikes_yn1, last_spikes1
@@ -77,11 +71,16 @@ def create_all_spikes1(V=V, dt=dt, V_spike=V_spike):
 V[:15, (0,202,402,602)] = -30
 V[:15,:50]=I[:15,:50]/tau_neuron*dt+V_rest
 
+''' Builing  the final potential matrix
+Create a higher value for artifical spike and 
+lower for artificial rest to be able to differenciate 
+them from the others'''
 def weight_related_result1(weight):
   all_spikes1, spikes_yn1, last_spikes1=create_all_spikes1()
   peaks=[]
-  for k in range (9900): #for one k looking at matrix
+  for k in range (9900): #for each time step
       W1,neuron_nr1=synaptic_weight1(layer_number,layer_size,weight) #matrix 150*150
+      #postsynaptic potential
       E = np.zeros(neuron_nr1) #now neuron_nr is 150
       for m in range(neuron_nr1):
         e = 0
@@ -90,18 +89,15 @@ def weight_related_result1(weight):
           et = (np.exp(-((k+2)*dt-f_times[n])/tau_synapse)) *np.sign(f_times[n])
           e=e+et 
         E[m] = e
-      
-      
-      I_syn=np.dot(W1,E) #vector 1*150?
+      I_syn=np.dot(W1,E) 
+      #noise
       noise=np.random.normal(noise_mean,noise_std,neuron_nr1)
-      #dv/dt
+      #Euler's equation
       derivV1=(-(V[:,k+1]-V_rest)+I_syn+I[:,k+1]+(noise_scale*noise))/tau_neuron
-      #reset if passes threshold
-      
+     
       for i in range(neuron_nr1):
           if V[i,k+2]<-40 and V[i,k+2]>-80:   
-              
-              V[i,k+2]=V[i,k+1]+dt*derivV1[i]
+              V[i,k+2]=V[i,k+1]+dt*derivV1[i] #apply equation when between artificial values
           elif V[i,k+2]==-80:
               V[i,k+2]=V_reset
           else:
@@ -113,20 +109,20 @@ def weight_related_result1(weight):
           if spikes_yn1[i]==1:
               if V[i,k+1]<-40: #if real spike
                   try:
-                      V[i,k+2]=V_reset
-                      V[i,(k+3):(k+1003)]=-80
-                      V[i,(k+202,k+402,k+602)]=-40
+                      V[i,k+2]=V_reset #reset at next time step
+                      V[i,(k+3):(k+1003)]=-80  #between artificial spikes stay at rest (will go to reset at next time step, and equation at the one just after)
+                      V[i,(k+202,k+402,k+602)]=-40 #create artificial spikes
                       
                   except IndexError:
                       pass
               elif V[i,k+1]>=-40:
-                   V[i,k+2]=V_reset
+                   V[i,k+2]=V_reset #if artificial spike, act like real spike and reset
           elif spikes_yn1[i]==0:
               V[i,k+2]=V[i,k+2]
          
       
-      spikes_yn1=(V[:,k+2]>=V_spike)
-    #record spikes
+      spikes_yn1=(V[:,k+2]>=V_spike) #spikes for this timestep
+      #record spikes
       B=np.argwhere(spikes_yn1==True)
       for j in range(len(spikes_yn1)):
         if spikes_yn1[j]==1:
@@ -139,6 +135,8 @@ def weight_related_result1(weight):
       peaks.append(B)
   return V, V1, peaks
 
+'''This function plots the potential evolution with the first layer in darkred,
+ and creates a raster plot of the spike times '''
 def plot_SYNFIRE1(weight):
   V, V1, peaks = weight_related_result1(weight)
   fig = plt.figure(figsize = (8,10))
@@ -153,7 +151,7 @@ def plot_SYNFIRE1(weight):
   ax = plt.subplot(2,1,2) #Raster
   plt.title("Spiking times for neurons")
   plt.ylim(0,layer_size*layer_number)
-  ax.set_xlabel("Time (ms)")
+  ax.set_xlabel("Time (s)")
   ax.set_ylabel("Neuron Index")
   
   spikes = []
@@ -162,20 +160,3 @@ def plot_SYNFIRE1(weight):
       plt.plot(t1[i],peaks[i][j], "|", color = "darkred")
 
   plt.tight_layout()
-  
-  
-  '''spikes_yn1=(V[:,k+2]>=V_spike)      
-  for i in range(len(spikes_yn1)): 
-                  
-      if spikes_yn1[i]==1: #at k+1
-                 #if V[i,k+1]<V_spike:
-         try:
-            V[i,k+2]=V_reset
-            V[i,(k+3):(k+1003)]=V_rest
-            V[i,(k+202,k+402,k+602)]=V_spike
-         except IndexError:
-            pass
-                 #elif V[i,k+1]>=V_spike:
-                  #  V[i,k+2]=V_reset
-      if spikes_yn1[i]==0:
-            V[i,k+2]=V[i,k+2]'''
